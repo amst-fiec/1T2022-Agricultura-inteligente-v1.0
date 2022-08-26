96169a99ba283308810afe685ef07b14d44367e3
Roberto <rjtorres@espol.edu.ec>
2022-08-26 09:16:44 -0500

    avance final

--- app/src/main/java/com/example/agriculturainteligente/menu_sensores.java
+++ app/src/main/java/com/example/agriculturainteligente/menu_sensores.java
@@ -2,6 +2,9 @@ package com.example.agriculturainteligente;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Context;
 import android.content.Intent;
@@ -18,90 +21,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.termometro_create.TermometroJhr;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import ingenieria.jhr.bluetoothjhr.BluetoothJhr;
 
 public class menu_sensores extends AppCompatActivity {
-
-    Boolean initHilo = false;
-    Boolean hilo = true;
-    BluetoothJhr blue;
-    String mensaje;
-    TextView display;
-    TermometroJhr termometroJhr;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_sensores);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        blue = new BluetoothJhr(this, Conexion.class);
-        display = findViewById(R.id.display);
-        termometroJhr = findViewById(R.id.termometroJhr);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navView, navController);
 
-
-        termometroJhr.tempMax(100);
-        termometroJhr.tempMin(0);
-        termometroJhr.setTimeAnimation(3000f);
-
-        new Thread(new Runnable () {
-            @Override
-            public void run() {
-                while (!initHilo){
-                    delay(500L);
-                }
-                while (hilo){
-                    blue.mTx("t");
-                    delay(1000L);
-                    mensaje = blue.mRx();
-                    if (mensaje!=""){
-                        if(hilo){
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    display.setText(mensaje);
-                                    termometroJhr.tempSet(Float.parseFloat(mensaje));
-                                }
-                            });
-                        }else{
-                            break;
-                        }
-                        blue.mensajeReset();
-                    }
-                    delay(1000L);
-                }
-                System.out.println("----------termino hilo---------");
-            }
-        }).start();
     }
 
-    public void delay(Long time){
-        try{
-            Thread.sleep(time);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        initHilo = blue.conectaBluetooth();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        hilo = false;
-        initHilo = true;
-        blue.exitConexion();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        initHilo = true;
-        hilo = false;
-    }
 }
